6377965bd8f412d890d6e8bee10508c012adc366
fcakyon <fcakyon@gmail.com>
2021-02-19 12:33:18 +0300

    make delay_sec configurable

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 silent_mode = 1  # 1/0
 language = "en"  # en/tr
 chromedriver_path = "/usr/local/bin/chromedriver"
+delay_sec = 2  # delay seconds btw requests
--- scrape.py
+++ scrape.py
@@ -1,9 +1,7 @@
 import config
 from scrape_utils import Earth2Scraper
 
-# init browser
 e2s = Earth2Scraper(config.silent_mode, config.language, config.chromedriver_path)
 e2s.init_browser()
-
 e2s.go_to_marketplace()
-_ = e2s.scrape_all_country_info(export=True, delay_sec=2)
+_ = e2s.scrape_all_country_info(export=True, delay_sec=config.delay_sec)
