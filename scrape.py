4e5b28a50db7ad8c49d436fb098b3d99300fb81d
fcakyon <fcakyon@gmail.com>
2021-02-20 18:53:20 +0300

    update delay_sec usage

--- scrape.py
+++ scrape.py
@@ -1,7 +1,9 @@
 import config
 from scrape_utils import Earth2Scraper
 
-e2s = Earth2Scraper(config.silent_mode, config.language, config.chromedriver_path)
+e2s = Earth2Scraper(
+    config.silent_mode, config.language, config.chromedriver_path, config.delay_sec
+)
 e2s.init_browser()
 e2s.go_to_marketplace()
-_ = e2s.scrape_all_country_info(export=True, delay_sec=config.delay_sec)
+_ = e2s.scrape_all_country_info(export=True)
